9127ff75e5bc59b14f50ab329975a9515af3c7f2
kaptsov <kaptsovaa@gmail.com>
2022-03-22 21:54:15 +0400

    Updated latest working version from replit

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -28,3 +29,16 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+    def get_duration(self):
+        delta = localtime(self.leaved_at) - localtime(self.entered_at)
+        return delta
+
+    def is_visit_long(self, minutes=60):
+        visit_minutes = ((self.get_duration().total_seconds()) // 60)
+        result = visit_minutes > minutes
+        return result
+
+    def format_duration(self):
+        minutes = ((self.get_duration().total_seconds()) % 3600) // 60
+        hours = (self.get_duration().total_seconds()) // 3600
+        return '{}h {}m'.format(int(hours), int(minutes))
